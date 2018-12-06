66187bcc5d4a1efdf61f6a289fd3c5d57aed6929
Logan Rakai <lmrakai@gmail.com>
2018-12-06 15:19:04 -0700

    Correct secret key name

--- src/credentials.py
+++ src/credentials.py
@@ -7,6 +7,6 @@ class credential_helper:
     def create_new_session():
         return boto3.Session(
         aws_access_key_id = CONFIG['client_id'],
-        aws_secret_access_key = CONFIG['client_key'],
+        aws_secret_access_key = CONFIG['client_secret'],
       	region_name = CONFIG['region_name']
     )
