39e487967fb49ae2fde5fd34c7a855717a9a119e
Logan Rakai <lmrakai@gmail.com>
2018-12-06 15:07:07 -0700

    Correct config dictionary name

--- src/credentials.py
+++ src/credentials.py
@@ -7,6 +7,6 @@ class credential_helper:
     def create_new_session():
         return boto3.Session(
         aws_access_key_id = CONFIG['client_id'],
-        aws_secret_access_key = event['client_key'],
-      	region_name = event['region_name']
+        aws_secret_access_key = CONFIG['client_key'],
+      	region_name = CONFIG['region_name']
     )
