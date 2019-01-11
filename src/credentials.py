62f0b5e1165b44d26c512c61fa75158d6043ae03
Logan Rakai <lmrakai@gmail.com>
2019-01-10 19:54:15 -0700

    Add missing self arg

--- src/credentials.py
+++ src/credentials.py
@@ -4,7 +4,7 @@ from config import CONFIG
 
 
 class credential_helper:
-    def create_new_session():
+    def create_new_session(self):
         return boto3.Session(
         aws_access_key_id = CONFIG['client_id'],
         aws_secret_access_key = CONFIG['client_secret'],
