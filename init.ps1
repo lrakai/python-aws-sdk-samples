868d5a1434248619349fffcbd85ddb58f639cc74
lrakai <lmrakai@gmail.com>
2021-03-03 16:14:17 -0700

    Fix typos

--- init.ps1
+++ init.ps1
@@ -5,7 +5,7 @@ $VCF_BITBUCKET_DIR="$VCF_BITBUCKET_PATH/labs-vcf-boilerplates"
 $CLOUD="azure" # valid values=aws|azure|gcp
 $REQUIREMENTS="$VCF_BITBUCKET_DIR/python3.6/$CLOUD.requirements.txt"
 
-If (!(test-path $CF_BITBUCKET_DIR)) {
+If (!(test-path $VCF_BITBUCKET_DIR)) {
     git config credential.helper store # store bitbucket credential on disk
     git clone $VCF_BITBUCKET_CLONE_URL $VCF_BITBUCKET_DIR
 } else {
--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ BITBUCKET_USER=YOUR_BITBUCKET_USER
 VCF_BITBUCKET_CLONE_URL=https://$BITBUCKET_USER@bitbucket.org/cloudacademy/labs-vcf-boilerplates.git
 VCF_BITBUCKET_PATH=..
 VCF_BITBUCKET_DIR=$VCF_BITBUCKET_PATH/labs-vcf-boilerplates
-CLOUD=azure # valid values=aws|azure|gcp
+CLOUD=aws # valid values=aws|azure|gcp
 REQUIREMENTS=$VCF_BITBUCKET_DIR/python3.6/$CLOUD.requirements.txt
 
 if [ ! -d "$VCF_BITBUCKET_DIR" ] ; then
